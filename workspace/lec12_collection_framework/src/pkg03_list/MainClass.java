2f962754b618decd16d638c1a5cc06f8417a02b8
WONDongin <bangsa100@naver.com>
2025-01-29 12:28:48 +0900

    [JAVA-28] List
    List

--- workspace/lec12_collection_framework/src/pkg03_list/MainClass.java
+++ workspace/lec12_collection_framework/src/pkg03_list/MainClass.java
@@ -1,6 +1,8 @@
 package pkg03_list;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MainClass {
@@ -37,5 +39,25 @@ public class MainClass {
 		
 		// 길이 확인
 		System.out.println(seasons.size());
+		
+		// 초기화 이용한 ArrayList 생성 (수정 불가)
+		List<String> sports = Arrays.asList("아구", "골프","볼링");
+		
+		// for 문을 이용한 ArrayList 순회
+		for(int i = 0,  size =   sports.size(); i < size; i++) {
+			System.out.println(sports.get(i));
+		}
+		
+		// 평균, 중간 값 구하기
+		List<Integer> numbers =  Arrays.asList(10, 20, 50, 60, 30, 70, 25, 55, 60);
+
+		int avg = (int) numbers.stream().mapToInt(Integer::intValue).average().orElse(0);
+		
+		Collections.sort(numbers);
+		int med = numbers.get(numbers.size() / 2);
+		
+		System.out.println(avg);
+		System.out.println(med);
+		
 	}
 }
